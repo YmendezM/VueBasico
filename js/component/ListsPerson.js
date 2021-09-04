2b3623f5a7a30d959c7b763c53a3f2f4a3ce3299
YmendezM <ysmendezm@gmail.com>
2021-09-04 10:34:09 -0400

    Manejo v-if, obtener index

--- js/component/ListsPerson.js
+++ js/component/ListsPerson.js
@@ -5,6 +5,11 @@ const ListsPerson = {
         <lis-alert></lis-alert>
         <ul>
             <li v-for="person in people">{{ person }}</li>
+            <!--Visualización de index en arreglo-->
+            <li v-for="(person, index) in people">
+            {{ index }} - {{ person }}
+            <span v-if="index == people.length -1"> - Este es el ultimo elemento</span>
+            </li>
         </ul>
     </div>`,
     data: function(){
